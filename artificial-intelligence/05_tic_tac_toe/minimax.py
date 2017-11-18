e968ed810f69d0c7fb8902c4b4d6601b731b703c
Iliya Zhechev <i4ko95@gmail.com>
2017-11-18 19:27:13 +0200

    cleanup

--- artificial-intelligence/05_tic_tac_toe/minimax.py
+++ artificial-intelligence/05_tic_tac_toe/minimax.py
@@ -6,7 +6,7 @@ class MinmaxPlayer:
         self.symbol = symbol
 
     def get_move(self, state):
-        pass
+        return [0, 1]
 
 
 class UserPlayer:
@@ -18,14 +18,14 @@ class UserPlayer:
 
 
 def get_start_state():
-    return ''.join([free_space] * 9)
+    return [free_space] * 9
 
 def print_state(state):
     print(state[:3])
     print(state[3:6])
     print(state[6:])
 
-def valid_move(state, move):
+def valid_move(move, state):
     x, y = move
     return state[x + y * 3] == free_space
 
@@ -36,30 +36,35 @@ def apply_move(move, state, symbol):
 
 def solved(state):
     is_solved = lambda group: len(set(group)) == len(group)
-    return all(
-        is_solved(state[:3]),
-        is_solved(state[3:6]),
-        is_solved(state[6:]),
-        is_solved([(i * 3) + 0 for i in range(3)])
-        is_solved([(i * 3) + 1 for i in range(3)])
-        is_solved([(i * 3) + 2 for i in range(3)])
-        is_solved([i * 3 + i for i in range(3)]) # Diagonal
-        is_solved([(i + 1) * 3 - (i + 1) for i in range(3)]) # Other diagonal
-    )
+    return all([
+        is_solved(state[:3]), is_solved(state[3:6]), is_solved(state[6:]),
+        is_solved([(i * 3) + 0 for i in range(3)]),
+        is_solved([(i * 3) + 1 for i in range(3)]),
+        is_solved([(i * 3) + 2 for i in range(3)]),
+        # Diagonal
+        is_solved([i * 3 + i for i in range(3)]),
+        # Other diagonal
+        is_solved([(i + 1) * 3 - (i + 1) for i in range(3)])
+    ])
 
 
-if __name__ == '__mani__':
+if __name__ == '__main__':
     players = UserPlayer('x'), MinmaxPlayer('o')
     current_state = get_start_state()
 
     while not solved(current_state):
+        print('==================')
+        print_state(current_state)
         current_player, _ = players
         move = current_player.get_move(current_state)
         if valid_move(move, current_state):
-            current_state = apply_move(move, current_state, current_player)
+            current_state = apply_move(
+                move, current_state,
+                current_player.symbol
+            )
             players = players[::-1]
         else:
             print('invalid move')
 
     _, winner = players
-    print('Winner is ' + winner)
+    print('Winner is %s' % winner)
