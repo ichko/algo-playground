f0eeea20b360dd53ff8adc5676dde4432b9894aa
Iliya Zhechev <i4ko95@gmail.com>
2017-11-24 16:11:50 +0200

    knn wip

--- artificial-intelligence/06_knn/knn.py
+++ artificial-intelligence/06_knn/knn.py
@@ -1,13 +1,26 @@
+from collections import Counter
+
+
+def sq_dist(a, b):
+    return sum((xa - xb) ** 2 for for xa, xb in zip(a, b))
+
+
+def mode(arr):
+    return Counter(arr).most_common(1)
+
+
 class KNN:
     def __init__(self, k):
         self.k = k
+        self.data = []
 
-    def fit(X=[]):
-        pass
-
+    def fit(X=[], y=[]):
+        self.data = [ex + [ey] for ex, ey in zip(X, y)]
 
     def predict(x):
-        pass
+        neighbors = sorted(self.data,
+            key=lambda: xy: sq_dist(xy[:-1], x))[:self.k]
+        return mode(ex[-1] for ex in neighbors)
 
 
 if __name__ == '__main__':
