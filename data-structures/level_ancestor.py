87bb2de97fb27bdaa22a5be349d7f4e4d7f6e39b
Iliya Zhechev <i4ko95@gmail.com>
2017-12-03 21:00:33 +0200

    ladder decomposition

--- data-structures/level_ancestor.py
+++ data-structures/level_ancestor.py
@@ -12,22 +12,52 @@ def count_sort(arr, key=lambda k: k):
     return ft.reduce(o.concat, [])
 
 
-def set_leaves(node, level=0):
-    node.level = level
+def get_leaves(node):
     if node.is_leaves():
-        self.leaves.append(node)
+        yield node
     for child in node.children:
-        set_leaves(child, level + 1)
+        yield from set_leaves(child)
+
+
+def max_path_decompose(sorted_leaves, root):
+    paths = [[]]
+    in_path = dd(lambda: True)
+    for leave in sorted_leaves:
+        node = leave
+        while node is not root and not in_path[node]:
+            in_path[node] = True
+            paths[-1].append(node)
+            node = node.parent
+
+    return paths
+
+
+def ladder_decompose(paths, root):
+    ladders = []
+    for path in paths:
+        ladder = []
+        node = path[0]
+        while node is not root and len(ladder) <= 2 * len(path):
+            ladder.append(node)
+            node = node.parent
+            
+        laders.append(ladder)
+
+    return ladders
 
 
 class Node:
-    def __init__(self, value, children = []):
+    def __init__(self, value):
         self.value = value
-        self.children = set(children)
+        self.parent = None
+        self.children = set()
         self.level = 0
 
     def add(self, children):
         self.children.update(children)
+        for child in children:
+            child.level = self.level + 1
+            child.parent = self
 
     def is_leave(self):
         return len(self.children) == 0
@@ -36,11 +66,13 @@ class Node:
 class Tree:
     def __init__(self, root):
         self.root = root
-        self.leaves = []
 
     def preprocess(self):
-        set_leaves(self.root)
-        self.leaves = count_sort(self.leaves, lambda l: l.level)
+        leaves = list(get_leaves(self.root))
+        leaves = count_sort(leaves, key=lambda l: l.level)
+        leaves.reverse()
+        paths = max_path_decompose(leaves, self.root)
+        ladder = ladder_decompose(paths)
 
 
     def level_ancestor(node, level):
@@ -48,11 +80,14 @@ class Tree:
 
 
 if __name__ == '__main__':
-    n = Node(4)
-    r = Node(
-        0,
-        [Node(1, Node(3, n)), Node(2)]
-    )
+    root = Node(0)
+    a_node = Node(1)
+    b_node = Node(1)
+    c_node = Node(1)
+    d_node = Node(1)
+    root.add(a_node, b_node)
+    a_node.add(c_node, d_node)
+
     t = Tree(t)
     t.preprocess()
-    print(t.level_ancestor(n, 2))
+    print(t.level_ancestor(d_node, 2) is root)
