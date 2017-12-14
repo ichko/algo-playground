f46913fa9a0d83f05a206caf5a50db6994d02e9b
Iliya Zhechev <i4ko95@gmail.com>
2017-12-14 22:38:26 +0200

    fit implementation

--- artificial-intelligence/07_naive_bayes/naive_bayes.py
+++ artificial-intelligence/07_naive_bayes/naive_bayes.py
@@ -1,30 +1,61 @@
 import csv
+from collections import defaultdict
 
 
 def read_file(file_name):
-    with open(file_name, newline='') as file:
-        data = list(csv.reader(file, delimiter=',', quotechar='\n'))
-        return [row[1:] for row in data], [row[0] for row in data]
+  with open(file_name, newline='') as file:
+    data = list(csv.reader(file, delimiter=',', quotechar='\n'))
+    return [row[1:] for row in data], [row[0] for row in data]
 
 
-def get_categories(data, column):
-    return set(row[column] for row in data)
+def get_cls_set(data, col):
+  return set(row[col] for row in data)
+
+
+def get_frequency(data, col_cls_list):
+  return len(1 for row in range(len(data)) if all(
+    row[col] == cls for col, cls in col_cls_list
+  ))
+
+
+def get_probability(data, col_a, col_b):
+  frequency = dict()
+
+  for cls_a in get_cls_set(data, col_a):
+    for cls_b in get_cls_set(data, col_b):
+      frequency[(cls_a, cls_b)] = get_frequency(
+        data, [(col_a, cls_a), (col_b, cls_b)]
+      )
+
+  return frequency
 
 
 class NaiveBayes:
-    def __init__(self):
-        pass
+  def __init__(self):
+    self.probabilities = []
+    self.label_classes = None
 
-    def fit(self, X, y):
-        pass
+  def fit(self, X, y):
+    data = [x + [y] for x, y in zip(X, y)]
+    self.label_classes = get_cls_set(data, -1)
+    for col in range(len(data[0])):
+      self.probabilities.append(
+        get_probability(data, col, -1)
+      )
 
-    def predict(self, X):
-        pass
+    return self
 
-    def score(self, X, y):
-        pass
+  def predict(self, X):
+    pass
+
+  def score(self, X, y):
+    pass
 
 
 if __name__ == '__main__':
-    X, y = read_file('voting.data')
-    print(y)
+  X, y = read_file('voting.data')
+
+  model = NaiveBayes().fit(X, y)
+  score = model.score(X, y)
+
+  print(score)
