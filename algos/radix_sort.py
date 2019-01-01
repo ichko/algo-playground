ee6d817352cd0b6855fa4e63c43cec49cdecae45
Iliya Zhechev <i4ko95@gmail.com>
2019-01-01 21:11:43 +0200

    Implement count sort with running sums

--- algos/radix_sort.py
+++ algos/radix_sort.py
@@ -1,6 +1,27 @@
 import unittest
-from random import randint
+from random import randint, seed
 from collections import defaultdict
+
+
+def counting_sort_running_sum(arr, key=lambda x: x):
+    if len(arr) <= 1:
+        return arr
+
+    max_item_key = max(map(key, arr))
+    pos = [0] * (max_item_key + 1)
+    output = [None] * len(arr)
+
+    for item in arr:
+        pos[key(item)] += 1
+
+    for i in range(1, max_item_key + 1):
+        pos[i] += pos[i - 1]
+
+    for item in arr[::-1]:
+        pos[key(item)] -= 1
+        output[pos[key(item)]] = item
+
+    return output
 
 
 def counting_sort(arr, key=lambda x: x):
@@ -29,7 +50,7 @@ def radix_sort(arr, key=lambda x: x):
     output = [(key(x), x) for x in arr]
 
     while max_item_key > 0:
-        output = counting_sort(output, lambda x: x[0] % base)
+        output = counting_sort_running_sum(output, lambda x: x[0] % base)
         output = [(x[0] // base, x[1]) for x in output]
         max_item_key //= base
 
@@ -37,6 +58,9 @@ def radix_sort(arr, key=lambda x: x):
 
 
 class TestSorting(unittest.TestCase):
+    def test_counting_sort_running_sum(self):
+        self.assert_testig_fnction(counting_sort_running_sum)
+
     def test_counting_sort(self):
         self.assert_testig_fnction(counting_sort)
 
@@ -48,7 +72,7 @@ class TestSorting(unittest.TestCase):
 
         for i in range(test_cases):
             input_size = randint(0, 20)
-            digits_size = 1000
+            digits_size = 10
             input_arr = [randint(0, digits_size) for _ in range(input_size)]
 
             actual_arr = sorting_function(input_arr)
@@ -58,4 +82,5 @@ class TestSorting(unittest.TestCase):
 
 
 if __name__ == "__main__":
+    seed(4)
     unittest.main()
